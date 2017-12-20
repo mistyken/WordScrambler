d2772de5dd916edf9f39e022f9d1f3c79ffa117f
nik167 <kenneth.chen@capitalone.com>
2017-12-20 15:37:43 -0800

    Adding functionality

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # WordScrambler
+Scramble words for fun!
+This is a simple app that shuffle all the letters between first and last.
--- app/src/main/java/mytest/wordscrambler/MainActivity.java
+++ app/src/main/java/mytest/wordscrambler/MainActivity.java
@@ -1,7 +1,15 @@
 package mytest.wordscrambler;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void scrambleMessage(View view){
+        EditText editText = (EditText) findViewById(R.id.textMessage);
+        String textMessage = editText.getText().toString();
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        
+        if(textMessage != null && textMessage.length() > 0){
+            String[] phrase = textMessage.split(" ");
+            for(String word: phrase){
+                char[] wordChar = word.toCharArray();
+                for(int i = 1; i < wordChar.length - 2; i++){
+                    int index = random.nextInt(i + 1) + 1;
+                    char temp = wordChar[index];
+                    wordChar[index] = wordChar[i];
+                    wordChar[i] = temp;
+                }
+                sb.append(String.valueOf(wordChar)).append(" ");
+            }
+        }
+
+        textMessage = sb.toString().toLowerCase();
+        
+        editText.setText(textMessage);
+        Toast toast = Toast.makeText(getApplicationContext(), "Text Scrambled", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void copyMessage(View view){
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        EditText editText = (EditText) findViewById(R.id.textMessage);
+        String textMessage = editText.getText().toString();
+        ClipData clip = ClipData.newPlainText("textMessage", textMessage);
+        clipboard.setPrimaryClip(clip);
+
+        Toast toast = Toast.makeText(getApplicationContext(), "Text Copied", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void clearMessage(View view){
+        EditText editText = (EditText) findViewById(R.id.textMessage);
+        editText.setText("");
+        Toast toast = Toast.makeText(getApplicationContext(), "Text Cleared", Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
